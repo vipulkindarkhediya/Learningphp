54f6836f6fd1f3789291f834e565dcdf17dc1d19
vckindarkhediya <vipulkindarkhediya1204@gmail.com>
2023-02-05 00:39:21 +0530

    data types

---
+++ Day-3-Learning/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$integer = 2323;
+
+$string = "Hello World";
+
+$float = 2.50;
+
+$boolean = true;
+
+$array = array("vipul", "Kamles", "ajaa");
+
+$null = null;
+
+
+echo "PHP Data types : <br>";
+echo "This is a integer :" . var_dump($integer) . "<br>";
+echo "This is a string :" . var_dump($string) . "<br>";
+echo "This is a float : (.)use float :" . var_dump($float) . "<br>";
+echo "This is a boolean : true = 1/false = 0 :" . var_dump($boolean) . "<br>";
+echo "This is a array :Multi values store :" . var_dump($array) . "<br>";
+echo "This is a null :Null variable is a not empty variable :" . var_dump($null) . "</br>";
+echo "THis is a object :One class create and object create use class";
