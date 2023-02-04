80b6a0cf9ab354b9eca086d350169fe60ea5821e
vckindarkhediya <vipulkindarkhediya1204@gmail.com>
2023-02-05 00:44:22 +0530

    assigment operator

---
+++ Day-7-Learning/index.php
@@ -0,0 +1,33 @@
+<?php
+
+$addtion = 50;
+$substraction = 20;
+$multiplication = 100;
+$division = 200;
+$modolus = 423;
+$exponentiation = 2;
+$increment = 20;
+$b = 10;
+$addtion += $b;
+$substraction -= $b;
+$multiplication *= $b;
+$division /= $b;
+$modolus %= $b;
+$exponentiation **= $b;
+
+
+
+
+echo "<h1>Assigment Operator</h1>";
+
+echo "This is a addition (+=) two values : " . $addtion  . "<br>";
+
+echo "This is a substraction (-=) two values : " . $substraction  . "<br>";
+
+echo "This is a multiplication (*=) two values : " . $multiplication  . "<br>";
+
+echo "This is a division (/=) two values : " . $division  . "<br>";
+
+echo "This is a modolus (%=) two values : " . $modolus  . "<br>";
+
+echo "This is a exponentiation (**=) two values : " . $exponentiation  . "<br>";
