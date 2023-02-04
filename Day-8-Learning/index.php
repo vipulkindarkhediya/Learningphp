0c2819f07374f93d4a1003dd0c5a1cc030664452
vckindarkhediya <vipulkindarkhediya1204@gmail.com>
2023-02-05 00:44:55 +0530

    comparison operator

---
+++ Day-8-Learning/index.php
@@ -0,0 +1,24 @@
+<?php
+
+echo "<h1>Comparison Operator</h1>";
+
+$a = "10";
+$b = 10;
+$c = 10;
+$d = 2;
+$e = 10;
+echo "This is a compare two value use a==b :  ", $a == $b, "<br>";
+
+echo "This is a compare two value use b===c : ", $b === $c, "<br>";
+
+echo "This is a compare two value use c!=d : ", $c != $d, "<br>";
+
+echo "This is a compare two value use a !== b : ", $a !== $b, "<br>";
+
+echo "This is a compare two value use c > d : ", $c > $d, "<br>";
+
+echo "This is a compare two value use d < c : ", $d < $c, "<br>";
+
+echo "This is a compare two value use c >= e : ", $c >= $e, "<br>";
+
+echo "This is a compare two value use e <= c : ", $e <= $c, "<br>";
