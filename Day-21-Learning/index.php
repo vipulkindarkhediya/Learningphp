72376744cfe6cb83d9dde22b78741ce736aeff18
vckindarkhediya <vipulkindarkhediya1204@gmail.com>
2023-02-06 16:54:54 +0530

    php function

---
+++ Day-21-Learning/index.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "<h1>php function</h1>";
+
+function vipul()  //function definition
+{
+     echo "Name : Vipul <br>";
+     echo "Roll number : 20 <br>";
+}
+
+echo "<h1>" . vipul() . "</h1>"; //calling function
+
+
+echo "<h3>" . vipul() . "</h3>"; //calling function
