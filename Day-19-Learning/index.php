fbb97633ad4c00c50826ee5491b5497206cb36ea
vckindarkhediya <vipulkindarkhediya1204@gmail.com>
2023-02-06 16:27:36 +0530

    break & continue statement

---
+++ Day-19-Learning/index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "<h1>Break & contrinue statement</h1>";
+
+
+for ($i = 0; $i <= 20; $i++) {
+     if ($i == 10) {
+          echo "This used continue statement other user show Not user show : " . $i . "<br>";
+          continue;
+     } elseif ($i == 15) {
+          echo "This used break statement other user not show same user succefully : " . $i . "<br>";
+          break;
+     } else {
+          echo "This continue statement : " . $i . "<br>";
+     }
+}
