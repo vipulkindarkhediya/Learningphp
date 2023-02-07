c2c7958b403beb517cf6691702032d000381432b
vckindarkhediya <vipulkindarkhediya1204@gmail.com>
2023-02-07 09:30:59 +0530

    function with parameter

---
+++ Day-23-Learning/index.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "<h1>function with return value</h1>";
+
+function student($name, $age, $contact)
+{
+     return "Name is : " . $name . "<br>" . "Age is : " . $age . "<br> " . "Contact is : " . $contact . "<br><br>";
+}
+
+echo student("vipul", 23, 9879663850);
+echo student("Sagar",12,3456789012);
