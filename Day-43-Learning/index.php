bfc310cbf2230363e70b53432f8ccd2de3b4732e
vckindarkhediya <vipulkindarkhediya1204@gmail.com>
2023-02-08 10:51:17 +0530

    php time()

---
+++ Day-43-Learning/index.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "<h1>php time()</h1>";
+
+
+echo "<strong>Hour's formates is :</strong> <br>First : "  .  date("h") . "<br>Second : " . date("H") . "<br>Thard : " . date("g") . "<br>Four : " . date("G") . "<br><br>";
+
+echo "<strong>Minutes formates is :</strong> <br>First : "  .  date("i") . "<br><br>";
+
+echo "<strong>Second's formates is :</strong> <br>First : "  .  date("s") . "<br><br>";
+
+echo "<strong>Meridiem formates is :</strong> <br>First : "  .  date("a") . "<br>Second : " . date("A") . "<br><br>";
