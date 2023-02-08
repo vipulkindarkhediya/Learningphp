3819a77313ac779ca27ce4d4d23d0c3029297b1b
vckindarkhediya <vipulkindarkhediya1204@gmail.com>
2023-02-08 14:22:40 +0530

    php date_create() & date_formate()

--- Day-45-Learning/index.php
+++ Day-45-Learning/index.php
@@ -4,7 +4,7 @@ echo "<h1>php date_create() & date_formate()</h1>";
 
 echo "<h3>date_create()</h3>";
 
-$date = date_create("2024-6-02");
+$date = date_create("2024-6-02 22:16:30", timezone_open("Asia/kolkata"));
 
 
 echo "<h3>date_formate()</h3>";
