7afb5b58ac8467c33414b2b921d97919f5607407
vckindarkhediya <vipulkindarkhediya1204@gmail.com>
2023-02-19 16:32:26 +0530

    php include_once & require_once

---
+++ Day-54-Learning/index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<h1>php include_once & require_once</h1>";
+
+echo "<br><h2>include_once :- include_once used one file include two output only one  </h2><br>";
+
+
+echo "<h2>require_once :- require_once used one file require two output only one  </h2>";
+
+?>
