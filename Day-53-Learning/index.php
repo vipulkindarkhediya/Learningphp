4d1b3f5d26fcaef82f3c5ca756d653f41cb21e67
vckindarkhediya <vipulkindarkhediya1204@gmail.com>
2023-02-19 16:20:53 +0530

    php include & require statement

---
+++ Day-53-Learning/index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<h1>php include & require statement</h1>";
+
+
+echo "<br><br><h2>include :- include used file add error show and other code run now</h2><br>";
+
+echo "<h2>require :- require used file add error show and other code not run </h2>";
+
+?>
