bd8f46f4012cf4b92542d697767b89c2d79e4403
vckindarkhediya <vipulkindarkhediya1204@gmail.com>
2023-02-20 11:13:24 +0530

    php get variable

---
+++ Day-55-Learning/get.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "Name get is ".$_GET['fname'];
+echo "<br>";
+echo "Age get is ".$_GET['age'];
---
+++ Day-55-Learning/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>php get variable</title>
+</head>
+
+<body>
+     <h1>php get variable</h1>
+     <form action="get.php" method="get">
+          <input type="text" name="fname" placeholder="Name"><br><br>
+          <input type="number" name="age" placeholder="Age"><br><br>
+          <button type="submit">get data</button>
+     </form>
+</body>
+
+</html>
