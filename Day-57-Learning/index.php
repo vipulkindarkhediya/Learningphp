beb180f9005fdcba4634c184346d09648a820a26
vckindarkhediya <vipulkindarkhediya1204@gmail.com>
2023-02-20 11:26:25 +0530

    php request variable

---
+++ Day-57-Learning/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>php request variable</title>
+</head>
+
+<body>
+     <h1>php request variable</h1>
+     <form action="request.php" method="post">
+          <input type="text" name="name" placeholder="Name">
+          <br><br>
+          <input type="number" name="age" placeholder="Age">
+          <br><br>
+          <button type="submit">request data</button>
+     </form>
+</body>
+
+</html>
---
+++ Day-57-Learning/request.php
@@ -0,0 +1,8 @@
+<?php
+
+echo $_REQUEST['name'] . "<br><br>";
+echo $_REQUEST['age'];
+
+?>
+<br><br>
+<a href="index.php">form</a>
