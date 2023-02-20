cda7978e5a9bc0464d9cbbc74b7120477a6a676f
vckindarkhediya <vipulkindarkhediya1204@gmail.com>
2023-02-20 11:13:59 +0530

    php post variable

---
+++ Day-56-Learning/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>php post variable</title>
+</head>
+<body>
+     <h1>php post variable</h1>
+     <form action="post.php" method="post">
+          <input type="text" name="name" placeholder="Enter name"><br><br>
+          <input type="number" name="age" placeholder="Enter age"><br><br>
+          <button type="submit">post data</button>
+     </form>
+</body>
+</html>
---
+++ Day-56-Learning/post.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "Name is : ".$_POST['name'];
+echo "<br>";
+echo "Age is : ".$_POST['age'];
